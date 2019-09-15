29df60c148be10a2da8ff98b608f991946e7b5da
Marcus Downing <marcus@bang-on.net>
2019-09-15 10:57:39 +0100

    Barbarian font!

--- src/lib/elements/section.js
+++ src/lib/elements/section.js
@@ -1,25 +1,27 @@
-import { elementClass, esc } from '../util';
+import { elementClass, esc, elementID } from '../util';
 import { __ } from '../i18n';
 
 export let section = {
-    name: 'section',
-    key: 'title',
-    defaults: {
-        title: '',
-        fill: false,
-        flex: 'medium',
-        untitled: false,
-        contents: [],
-    },
-    render: (args, reg, doc) => {
-        var cls = elementClass('section', null, args, ['primary', 'fill', 'untitled'], { flex: 'medium' });
+  name: 'section',
+  key: 'title',
+  defaults: {
+    title: '',
+    id: '',
+    fill: false,
+    flex: 'medium',
+    untitled: false,
+    contents: [],
+  },
+  render: (args, reg, doc) => {
+    var id = elementID('section', args.id);
+    var cls = elementClass('section', null, args, ['primary', 'fill', 'untitled'], { flex: 'medium' });
 
-        var title = args.untitled ? '' : `<h3>${esc(__(args.title, doc))}</h3>`;
-        // var content = (args.contents.length == 1 && args.contents[0].type == "g") ? render(args.contents) : `<div class='g'>${render(args.contents)}</div>`;
-        var content = `<div class='section__inner'>${reg.render(args.contents, doc)}</div>`;
+    var title = args.untitled ? '' : `<h3>${esc(__(args.title, doc))}</h3>`;
+    // var content = (args.contents.length == 1 && args.contents[0].type == "g") ? render(args.contents) : `<div class='g'>${render(args.contents)}</div>`;
+    var content = `<div class='section__inner'>${reg.render(args.contents, doc)}</div>`;
 
-        return `<section${cls}>${title}
+    return `<section${id}${cls}>${title}
             ${content}
             </section>`;
-    }
+  }
 }
