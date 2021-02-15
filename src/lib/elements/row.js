8b37b6e73efa3268c9acf000a655b9c8e7405cc0
Marcus Downing <marcus.downing@gmail.com>
2021-02-15 22:09:37 +0000

    Martial Artist

--- src/lib/elements/row.js
+++ src/lib/elements/row.js
@@ -14,7 +14,7 @@ export let row = {
   render(args, reg, doc) {
     // args.lp = getLabelHeight(args);
     args.rb = getRubyHeight(args);
-    const cls = elementClass('row', null, args, ['unlabelled', 'narrow', 'blk'], { 'layout': 'left', 'valign': 'bottom', 'rb': '', 'flex': 'medium' });
+    const cls = elementClass('row', null, args, ['unlabelled', 'narrow', 'blk', 'pad'], { 'layout': 'left', 'valign': 'bottom', 'rb': '', 'flex': 'medium' });
     return `<div${cls}><div class='row__inner'>${reg.render(args.contents, doc)}</div></div>`;
   }
 }
